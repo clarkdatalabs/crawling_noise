5c477c6cc7bf74ec627f89008e7d8ff839d017aa
jjoque <joque@umich.edu>
2016-10-20 15:43:13 -0400

    Update send.py

--- send.py
+++ send.py
@@ -29,7 +29,7 @@ while (1==1):
         c.send(oscmsg)
 
     except:
-        print "bad url :" + url
+        print "url didn't work:" + url
         url = stack[0]
         stack = stack[1:]
         continue
