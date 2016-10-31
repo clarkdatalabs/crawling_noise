e83878e2d7eaa7645982997bac45eaf662fb79e3
Justin Joque <joque@umich.edu>
2016-10-31 10:59:01 -0400

    fixed a lot of bad urls by changing ['content-length'] to read().__sizeof__()

--- send.py
+++ send.py
@@ -4,6 +4,7 @@ import urllib2
 from bs4 import BeautifulSoup
 from urlparse import urljoin
 import re
+import sys
 
 
 def TLDcode(url):   #checks Top Level Domain. .edu -->0, .com -->1, <anything else> --> 2
@@ -11,7 +12,7 @@ def TLDcode(url):   #checks Top Level Domain. .edu -->0, .com -->1, <anything el
         return 0                            #returns 0 for .edu or .edu/... endings
     elif  re.search('\.com/|\.com$',url):
         return 1                            #returns 1 for .com or .com/... endings
-    else:                                       
+    else:
         return 2                            #returns 2 for everything else
 
 url = "http://www.umich.edu"
@@ -19,44 +20,49 @@ stack = []
 visted = []
 
 while (1 == 1):
-    
+
     try:
         f = urllib2.urlopen(url)
 
         soup = BeautifulSoup(f, 'html.parser')
-        links = soup.find_all('a', href=True)
-        divs = soup.find_all('div')
-        for link in links:
-            href = link.get('href')
-            joinurl = urljoin(url, href)
-            if joinurl.startswith("http") and joinurl not in visted:
-                stack.append(joinurl) 
-                visted.append(joinurl)
-        stack.append("newpage")       #once all links are added, insert a <newpage> counter into the stack
-
-
-        c = OSC.OSCClient()
-        c.connect(('127.0.0.1', 57120))  # connect to SuperCollider
-        oscmsg = OSC.OSCMessage()
-        oscmsg.setAddress("/startup")  # the name of the channel  ///once exhausting a page, send a new page event
-        oscmsg.append(len(links))
-        oscmsg.append(f.headers["Content-Length"])  ## size of the file
-        oscmsg.append(len(divs))
-        oscmsg.append(TLDcode(url))     # Top Level Domain (.edu=0, .com=1, other=2)
-        c.send(oscmsg)
 
     except:
+        print "error:", sys.exc_info()[0]
         print "bad url:" + url
         url = stack[0]
         stack = stack[1:]
         continue
+
+    links = soup.find_all('a', href=True)
+    divs = soup.find_all('div')
+    for link in links:
+        href = link.get('href')
+        joinurl = urljoin(url, href)
+        if joinurl.startswith("http") and joinurl not in visted:
+            stack.append(joinurl)
+            visted.append(joinurl)
+    stack.append("newpage")       #once all links are added, insert a <newpage> counter into the stack
+
+
+    c = OSC.OSCClient()
+    c.connect(('127.0.0.1', 57120))  # connect to SuperCollider
+    oscmsg = OSC.OSCMessage()
+    oscmsg.setAddress("/startup")  # the name of the channel  ///once exhausting a page, send a new page event
+    oscmsg.append(len(links))
+    oscmsg.append(f.read().__sizeof__())  ## size of the file
+    oscmsg.append(5)
+    oscmsg.append(TLDcode(url))     # Top Level Domain (.edu=0, .com=1, other=2)
+    c.send(oscmsg)
+
+    print url
+
 
     url = stack[0]
     if url == "newpage": ## whenever we go to the new page, send a message says new page.
 #         print "newpage"
         stack = stack[1:]
         url = stack[0]
-        
+
         c = OSC.OSCClient()
         c.connect(('127.0.0.1', 57120))  # connect to SuperCollider
         oscmsg = OSC.OSCMessage()
@@ -65,4 +71,3 @@ while (1 == 1):
         c.send(oscmsg)
 
     stack = stack[1:]
-
